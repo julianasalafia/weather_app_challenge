56bf9a34a4b7285fd2c3391109757e3aa0e70709
julianasalafia <juliana.salafia1@gmail.com>
2023-11-01 15:38:35 -0300

    feat: create utils folder with app colors and constants file

---
+++ lib/app/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color purple = Color(0xFF352163);
+  static const Color midPurple = Color(0xFF331972);
+  static const Color darkerPurple = Color(0xFF33143C);
+}
---
+++ lib/app/utils/constants.dart
@@ -0,0 +1,16 @@
+const double toolbarHeight = 60.0;
+
+// ICONS
+const backIconButton = 'assets/icons/back_button.png';
+const reloadIconButton = 'assets/icons/reload_button.png';
+const menuIconButton = 'assets/icons/menu_button.png';
+const backgroundImage = 'assets/images/background_image.png';
+
+const kIconSize = 35.0;
+const kSizedBoxHeight = 10.0;
+const kPaddingHorizontal = 10.0;
+
+// TOOLBAR
+const toolbarSearchTitle = 'Search for City';
+const toolbarWeekStatusTitle = '7 Dias';
+const toolbarInformationTitle = 'Informações';
